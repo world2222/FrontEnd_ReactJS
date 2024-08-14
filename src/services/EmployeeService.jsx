b9a30ae3f84bdcfc3c96bc7890bd782402d03ba7
HyunJong Kim <world2222222222@gmail.com>
2024-08-14 10:12:21 +0900

    Implement HeaderComponent.jsx

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -5,6 +5,7 @@ const REST_API_BASE_URL = import.meta.env.VITE_APIURL   // original: 'http://loc
 const REST_API_URL = `${REST_API_BASE_URL}/employees`
 //'http://localhost:8080/api/employees';
 
+
 //export const listEmployees = () => axios.get(REST_API_BASE_URL);
 export const listEmployees = () => axios.get(`${REST_API_URL}/departments`);
 
